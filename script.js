b786219a4bcc41358e7494082f9e11565747dfe5
Rafael Macedo Rapacci <rafael.rapacci@sptech.school>
2022-03-23 23:18:32 -0300

    MK-Project-v3
    Added new buttons  and new animations

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ function noOutline() {
     id_img_but.style.boxShadow = "none"
 }
 
+function outline_soco1() {
+    button_soco1.style.boxShadow = "0 0 20px red"
+}
+
+function noOutline_soco1() {
+    button_soco1.style.boxShadow = "0 0 30px black"
+}
+
+function outline_soco2() {
+    button_soco2.style.boxShadow = "0 0 20px red"
+}
+
+function noOutline_soco2() {
+    button_soco2.style.boxShadow = "0 0 30px black"
+}
+
+
 function iniciar() {
     id_img_but.style.boxShadow = "0 0 20px black"
     id_img_but.style.boxShadow = "0 0 20px red"
@@ -22,10 +39,8 @@ function iniciar() {
     hp_scorp.style.borderStyle = "solid"
     hp_sub.style.width = "510px"
     hp_sub.style.borderStyle = "solid"
+    button_soco1.style.opacity = "1"
+    button_soco2.style.opacity = "1"
     audio_mk.play()
-
-    
-    /*div1.innerHTML = `<img class="scorp" src="scorpion.png">`
-    div2.innerHTML = `<img class="sub" src="subzero.png">`*/
 }
 
